55d350ce2d4366b7a021eb9fc85ce9cad2454b86
Vurich <jackefransham@hotmail.co.uk>
2017-07-19 17:59:18 +0200

    Make unrolling macro more compact

--- gen-code.sh
+++ gen-code.sh
@@ -5,16 +5,18 @@ set -euo pipefail
 echo '#[macro_export]'
 echo "
   macro_rules! unroll {
-    (for \$var:ident in 0..0 \$blk:block) => {};
+    (for \$var:ident in 0..\$to:tt \$blk:block) => {
+         unroll! { @for \$var \$to \$to \$blk }
+    };
+    (@for \$var:ident \$to:tt 0 \$blk:block) => {};
     $(
       for i in $(seq 1 $1); do
         echo "
-        (for \$var:ident in 0..$i \$blk:block) => {
+        (@for \$var:ident \$to:tt $i \$blk:block) => {
           $(
             echo '#[allow(non_upper_case_globals)]{'
-            for j in $(seq 0 $((i - 1))); do
-              echo "            { const \$var: usize = $j; \$blk }"
-            done
+            echo "            { const \$var: usize = \$to - $i; \$blk }"
+            echo "            unroll! { @for \$var \$to $((i - 1)) \$blk }"
             echo '          }'
           )
         };
@@ -23,3 +25,17 @@ echo "
     )
   }
 "
+echo '
+#[test]
+fn test() {
+    let mut a = vec![];
+
+    unroll! {
+        for i in 0..5 {
+            a.push(i);
+        }
+    }
+
+    assert_eq!(a, vec![0, 1, 2, 3, 4]);
+}
+'
