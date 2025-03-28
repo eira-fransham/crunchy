1ec13bee94b5b5691ef653154e3c8fa682ea676a
Serentty <noname422@gmail.com>
2025-03-27 20:16:15 -0400

    Switch to forward slash for lib.rs path
    Windows supports forward slashes as directory separators. The use of a backslash here breaks building for Windows on Linux.

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,4 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(target_os = "windows")]
-include!(concat!(env!("OUT_DIR"), "\\lib.rs"));
-
-#[cfg(not(target_os = "windows"))]
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
